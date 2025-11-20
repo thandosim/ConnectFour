62c97de3ee7f59920fd0a59e5bc32dc71c8c653e
thandosim <sim24001@byui.edu>
2025-11-20 08:53:57 +0200

    Update static web assets and add Board component styles
    - Updated staticwebassets.build.json and its cache to reflect new asset hashes.
    - Added Board.razor.css with styles for the game board and player pieces.
    - Generated scoped CSS for the Board component with unique identifiers for styles.

--- obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0e5d05fc3374e27d3cbd4e904629a1bce1e674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c47820eeda7b2c0c371105a1b684ff1a39cced7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
