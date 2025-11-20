4a419d27a9e18fb9cfcac6c0f8369bb71fb68a75
thandosim <sim24001@byui.edu>
2025-11-20 10:16:24 +0200

    Update static web assets manifest and cache with new hash values and asset fingerprints

--- obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4259703c1142eb91d90b2a0d6a5b96c408a085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ee9ca4308eb2586b8d55a560c078df9231beeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
